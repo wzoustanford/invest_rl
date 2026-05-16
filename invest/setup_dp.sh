7475d1c5c0426fd4e725c0fedcb201a5c8a7dd9f
Will Y. Zou <wzou@alumni.stanford.edu>
2026-05-16 10:36:12 -0700

    Replace AWS access keys with placeholders
    Updated AWS credentials in setup_dp.sh to use placeholder values.

--- invest/setup_dp.sh
+++ invest/setup_dp.sh
@@ -7,7 +7,7 @@ git clone https://wzoustanford:xxx@github.com/wzoustanford/angle_rl.git
 
 mkdir ~/.aws/
 touch ~/.aws/credentials 
-echo -e "[default]\naws_access_key_id = AKIASDQ36EOJONW2RDHZ\naws_secret_access_key = xxx" > ~/.aws/credentials
+echo -e "[default]\naws_access_key_id = xxx\naws_secret_access_key = xxx" > ~/.aws/credentials
 cd angle_rl/invest/data 
 
 #aws s3 cp --recursive s3://illumenti-backend-general/angle_rl_data/dec23_to_mar24_benchmark_data/ ./
